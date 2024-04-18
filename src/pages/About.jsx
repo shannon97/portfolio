9e10ea30ba5a4aef4a1a7fc25f91361bd1d65a15
Shannon Oliver <shannon-alexis@hotmail.com>
2024-04-18 11:00:59 +0800

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -86,19 +86,19 @@ import { ChevronDoubleDownIcon } from "@heroicons/react/24/solid";
 const sections = [
     {
         title: "Personal Summary",
-        content: "I'm a creative soul with a passion for technology and a drive to explore the world. My journey in tech began not too long ago, and every day brings a new learning opportunity. I'm currently honing my coding skills through the She Codes Plus Program, and I aspire to make a mark in IT. When I'm not coding, you can find me planning my next travel adventure—I'm always ready for a new experience!"
+        content: "I'm a creative soul with a passion for technology and a drive to explore the world. My journey in tech brings a new learning opportunity every day.<br><br>I'm currently honing my coding skills through the She Codes Plus Program, and I aspire to make a mark in IT. When I'm not coding, you can find me planning my next travel adventure—I'm always ready for a new experience!"
     },
     {
         title: "Professional Summary",
-        content: "Need someone to boost your team’s technical prowess? I’m your person! With a diverse background that spans eLearning, ICT helpdesks, office management, and customer service, I thrive in dynamic settings. My strong communication skills, keen eye for detail, and teamwork spirit are my strengths. I’m passionate about resolving challenges, delighting customers, and enhancing workplace efficiency. You can count on me to bring energy and professionalism to every project."
+        content: "Need someone to boost your team’s technical expertise? I’m your person! With a diverse background that spans eLearning, ICT helpdesks, team management, and customer service, I thrive in dynamic settings.<br><br>My strong communication skills, keen eye for detail, and teamwork spirit are my strengths. I’m passionate about resolving challenges, delighting customers, and enhancing workplace efficiency. You can count on me to bring energy and professionalism to every project."
     },
     {
         title: "Key Attributes",
-        content: "System Expertise: I ensure systems are set up flawlessly for optimal use. Problem Solver: I excel at diagnosing and addressing complex problems before they escalate. Training Enhancement: I create educational materials that elevate the user experience. Efficient Multitasker: I effectively juggle multiple tasks without missing a beat. Tailored Communicator: I communicate clearly and adjust my style to engage various audiences. Continuous Improvement: I'm always learning and staying abreast of new trends in the industry."
+        content: "System Expertise: I ensure systems are set up flawlessly for optimal use.<br>Problem Solver: I excel at diagnosing and addressing complex problems before they escalate.<br>Training Enhancement: I create educational materials that elevate the user experience.<br>Efficient Multitasker: I effectively juggle multiple tasks without missing a beat.<br>Tailored Communicator: I communicate clearly and adjust my style to engage various audiences.<br>Continuous Improvement: I'm always learning and staying abreast of new trends in the industry."
     },
     {
         title: "Industry Interests",
-        content: "I'm passionate about web design, wireframe creation, frontend coding, and UX/UI. I strive to blend aesthetics with functionality, keeping up with the latest trends to deliver optimal user experiences. My goal is to develop intuitive and visually engaging digital products."
+        content: "I'm passionate about web design, wireframe creation, frontend coding, and UX/UI. I strive to blend aesthetics with functionality, keeping up with the latest trends to deliver optimal user experiences.<br><br>My goal is to develop intuitive and visually engaging digital products that make people say 'WOW'."
     }
 ];
 
